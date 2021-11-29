9ff4abd2930ad10d732b7d714d9d037a61e2cec5
victorrochaaudaces <victor.rocha@audaces.com>
2021-11-29 10:19:07 -0300

    init git 29/11 2.0

---
+++ src/entidadeDao/FuncionarioDao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidadeDao;
+
+import Entidade.Funcionario;
+import java.util.List;
+
+/**
+ *
+ * @author victor.rocha
+ */
+public interface FuncionarioDao {
+     public abstract void salvar(Funcionario funcionario) throws Exception;
+
+    public abstract void alterar(Funcionario funcionario) throws Exception;
+
+    void excluir(int id) throws Exception;
+
+    Funcionario pesquisarPorId(int id) throws Exception;
+
+    List<Funcionario> pesquisarPorNome(String nome) throws Exception;
+
+    Funcionario logar(String email, String senha) throws Exception;
+}
