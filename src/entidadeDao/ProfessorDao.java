6d8a87b5155207d691ad82cff8d46fb121fd856d
Victor rodrigues da rocha <89555576+victorrochaaudaces@users.noreply.github.com>
2021-11-29 15:14:11 -0300

    Add files via upload

---
+++ src/entidadeDao/ProfessorDao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidadeDao;
+
+import Entidade.Professor;
+import java.util.List;
+
+/**
+ *
+ * @author victor.rocha
+ */
+public interface ProfessorDao {
+
+    public abstract void salvar(Professor professor) throws Exception;
+
+    public abstract void alterar(Professor professor) throws Exception;
+
+    void excluir(int codProf) throws Exception;
+
+    Professor pesquisarPorId(int codProf) throws Exception;
+
+    List<Professor> pesquisarPorNome(String nmProf) throws Exception;
+}
